a456ab0345301547acabe18f4e2bffbe81434428
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2020-03-09 10:08:22 -0700

    Refactoring

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 const mysql = require("mysql");
 const dotenv = require('dotenv');
 dotenv.config();
-const connection = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: "burgers_db"
-});
+})};
 
 // Make connection.
 connection.connect(function(err) {
