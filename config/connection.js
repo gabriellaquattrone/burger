5df74498bb41c6b554efdd6009858d00fa29e936
Gabriella Quattrone <gfquattrone@ucdavis.edu>
2020-03-09 19:06:58 -0700

    Fixing port

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL){
 } else {
  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: process.env.PORT,
     user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: "burgers_db"
